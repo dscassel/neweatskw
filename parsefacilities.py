4ec01160770d347d01464be153dc8b9e684c185e
Darcy Casselman <dscassel@gmail.com>
2014-02-22 22:09:53 -0500

    stores already-seen restaurants in an sqlite database.

--- parsefacilities.py
+++ parsefacilities.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 
 from bs4 import BeautifulSoup
+import sqlite3
+
+db = sqlite3.connect('restaurants.db')
+
+cursor = db.cursor()
 
 facilities = BeautifulSoup(open("testdata/doc.kml"), "xml")
 
@@ -14,7 +19,27 @@ for place in facilities.find_all("Placemark"):
 	cells = [x.string for x in row.find_all("td")]
 	details[cells[0]] = cells[1]
 
-    print details["CategoryStyle"]
+    if details['CategoryStyle'] in ('Restaurant', 'Food Take Out') \
+	and details['SiteCity'] in ('WATERLOO', 'KITCHENER', 'ST.+JACOBS'):
 
+	cursor.execute("SELECT * FROM facilities WHERE id=?;", (details['FacilityMasterID'],))
+
+	if cursor.fetchone() is None:
+	    print str.format("{}: {}, {}", details['FacilityName'], details['SiteStreet'], details['SiteCity'])
+
+    
+    cursor.execute('''
+	INSERT OR REPLACE INTO facilities (id, name, lastupdate, creation)
+	    VALUES ( ?, ?, ?, ? );''', 
+	(details['FacilityMasterID'], 
+	details['FacilityName'], 
+	details['LastUpdateDateTime'],
+	details['CreationDateTime']))
+
+db.commit()
+db.close()
+			
+	
+	
 
 		
