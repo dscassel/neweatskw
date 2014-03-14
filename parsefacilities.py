2ef515ac73659ee3b8c6ea566c04ec69dff01a9e
Mark Sherry <mdsherry@gmail.com>
2014-03-13 21:25:10 -0400

    Extensive refactoring; adding version field to DB.

--- parsefacilities.py
+++ parsefacilities.py
@@ -2,44 +2,77 @@
 
 from bs4 import BeautifulSoup
 import sqlite3
+import os
+import datetime
 
-db = sqlite3.connect('restaurants.db')
+def getColStrings( tr ):
+	return tuple( col.string for col in tr.find_all("td") )
 
-cursor = db.cursor()
+def getFacilities():
+	facilities = BeautifulSoup(open("testdata/doc.kml"), "xml")
 
-facilities = BeautifulSoup(open("testdata/doc.kml"), "xml")
+	for place in facilities.find_all("Placemark"):
+		description = BeautifulSoup(''.join(place.description.contents))
+		table = description.table.table
+		
+		rows = table.find_all("tr")
+		details = dict( getColStrings( row ) for row in rows)
+		yield details
 
 
-for place in facilities.find_all("Placemark"):
-    description = BeautifulSoup(''.join(place.description.contents))
-    table = description.table.table
-    details = dict()
+def doDBStuff(cursor, details):
 
-    for row in table.find_all("tr"):
-	cells = [x.string for x in row.find_all("td")]
-	details[cells[0]] = cells[1]
+	if details['CategoryStyle'] in ('Restaurant', 'Food Take Out') \
+		and details['SiteCity'] in ('WATERLOO', 'KITCHENER', 'ST.+JACOBS'):
 
-    if details['CategoryStyle'] in ('Restaurant', 'Food Take Out') \
-	and details['SiteCity'] in ('WATERLOO', 'KITCHENER', 'ST.+JACOBS'):
+		cursor.execute("SELECT * FROM facilities WHERE id=?;", (details['FacilityMasterID'],))
 
-	cursor.execute("SELECT * FROM facilities WHERE id=?;", (details['FacilityMasterID'],))
-
-	if cursor.fetchone() is None:
-	    print str.format("{}: {}, {}", details['FacilityName'], details['SiteStreet'], details['SiteCity'])
+		if True or cursor.fetchone() is None:
+			print str.vformat("{FacilityName}: {SiteStreet}, {SiteCity}", details )
 
     
-    cursor.execute('''
-	INSERT OR REPLACE INTO facilities (id, name, lastupdate, creation)
-	    VALUES ( ?, ?, ?, ? );''', 
-	(details['FacilityMasterID'], 
-	details['FacilityName'], 
-	details['LastUpdateDateTime'],
-	details['CreationDateTime']))
-
-db.commit()
-db.close()
-			
+	cursor.execute('''
+	INSERT OR REPLACE INTO facilities (id, name, lastupdate, creation, firstseen, lastseen)
+		VALUES ( ?, ?, ?, ?, ?, ? );''', 
+		(details['FacilityMasterID'], 
+		details['FacilityName'], 
+		details['LastUpdateDateTime'],
+		details['CreationDateTime'],
+		datetime.datetime.now(),
+		datetime.datetime.now()))
 	
-	
+def createDB( cursor):
+	cursor.execute("CREATE TABLE facilities (id, name, lastupdate, creation, firstseen, lastseen);")
+	cursor.execute("CREATE TABLE settings (key, value);")
+	cursor.execute("INSERT INTO settings VALUES ('version', 1);")
 
+
+def tryUpgradeDB( cursor ):
+	try:
+		cursor.execute("SELECT * from settings;");
+	except sqlite3.OperationalError as ex:
+		print ("Original database detected; upgrading")
+		cursor.execute("CREATE TABLE settings (key, value);")
+		cursor.execute("INSERT INTO settings VALUES ('version', 1);")		
+		cursor.execute("ALTER TABLE facilities ADD COLUMN firstseen;")
+		cursor.execute("ALTER TABLE facilities ADD COLUMN lastseen;")
+
+def main():
+	# Track whether we need to create the DB or not.
+	noDB = not os.path.exists( 'restaurants.db' )
+	db = sqlite3.connect( 'restaurants.db' )
+	cursor = db.cursor()
+	
+	if noDB:
+		createDB( cursor )
+		db.commit()
+	else:
+		tryUpgradeDB( cursor )
+
+	for facility in getFacilities():
+		doDBStuff( cursor, facility )
+	db.commit()
+	db.close()
 		
+if __name__ == '__main__':
+	main()
