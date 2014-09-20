e781a73a12e06d45f5499af10dc20ab5f573bb91
Darcy Casselman <dscassel@gmail.com>
2014-09-20 00:14:58 -0400

    added enqueue to update script.

--- updateeats.sh
+++ updateeats.sh
@@ -4,3 +4,4 @@ wget http://www.regionofwaterloo.ca/opendatadownloads/Inspections.zip -O /tmp/In
 unzip -o /tmp/Inspections.zip -d testdata
 
 python parsefacilities.py --update 
+python parsefacilities.py --getrecent 1 --enqueue
